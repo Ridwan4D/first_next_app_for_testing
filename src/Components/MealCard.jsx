1ebaeeaabde445168cfee1d8707bd12d5381afa3
Ridwanul Islam <ridwanul161@gmail.com>
2024-11-29 18:33:09 +0300

    meal card updated

--- src/Components/MealCard.jsx
+++ src/Components/MealCard.jsx
@@ -5,30 +5,25 @@ import React from "react";
 const MealCard = ({ meal }) => {
   return (
     <div>
-      <Link
-        href={`/meals/${meal?.idMeal}`}
-        key={meal?.idMeal}
-        className="bg-white shadow-md max-w-96 rounded-lg overflow-hidden transition-transform transform hover:scale-105 block"
-      >
-        <div className="relative w-full max-h-96 h-auto">
-                <Image
-                  src={meal?.strMealThumb}
-                  alt={meal?.strMeal}
-                  width={500}
-                  height={500}
-                  className="rounded-t-lg w-full"
-                />
-              </div>
-        <div className="p-4">
-          <h2 className="text-lg font-semibold truncate">{meal?.strMeal}</h2>
-          <p className="text-sm text-gray-500 mb-2">
-            Category: {meal?.strCategory}
-          </p>
-          <p className="text-sm text-gray-700 line-clamp-3">
-            {meal?.strInstructions}
-          </p>
+      <div className="card bg-base-100 w-auto shadow-xl">
+        <figure>
+          <Image
+            src={meal?.strMealThumb}
+            alt={meal?.strMealThumb}
+            width={500}
+            height={500}
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{meal?.strMeal}</h2>
+          <p>{meal?.strInstructions.slice(0, 60)}</p>
+          <div className="card-actions justify-end">
+            <Link href={`/meals/${meal?.idMeal}`} className="btn btn-primary">
+              Details
+            </Link>
+          </div>
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
